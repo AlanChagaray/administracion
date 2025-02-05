0df06aed4ed9c0b0cf5c286e14783bc52a4aa904
AlanChagaray <alanandreschagaray@gmail.com>
2025-02-05 09:33:12 -0300

    ImportacionModel

--- src/app/services/vtaproductos.tsx
+++ src/app/services/vtaproductos.tsx
@@ -1,17 +1,5 @@
 import axios from "axios";
-import VtaProductos from '../api/vtaproductos/model';
 
-
-// export async function proveedoresBuscar(params = {}) {
-//     try {
-//       const { data } = await axios.get(process.env.NEXT_PUBLIC_API_URL + '/proveedores', { params });
-//       return Array.isArray(data) ? data : [];
-//     } catch (error) {
-//       console.error('Error al obtener los datos:', error);
-//       return [];
-//     }
-//   }
-  
  export async function vtaProductosObtener(idpedido: any) {
     try {
       const { data } = await axios.get(
